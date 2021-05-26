72963cca84c53995a3fd8762b56e66bb4c8eb474
= <manasmhapuskar@gmail.com>
2021-05-25 23:51:40 -0500

    Deleted Notes files, Added the Problem Question in the HTML file

--- 01_Basics/01_Creating_Variables/01-location-tracker.js
+++ 01_Basics/01_Creating_Variables/01-location-tracker.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+Let's put your knowledge on creating variables into practice
+and help a travel journaling app keep track of a traveler's location.
+
+
+Task:
+We'll create the variables for the country, state, and city
+the traveler is in and give them string values.
+*/
+
+
 var continent = "North America";
 var country = "USA";
 var state = "Florida";
--- 01_Basics/01_Creating_Variables/02-game-character-name.js
+++ 01_Basics/01_Creating_Variables/02-game-character-name.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+Let's put your knowledge on 'Creating Variables' into practice
+and write an app on Mario, the famous video game character.
+
+
+Task:
+We'll create variables for Mario's name and job and use them
+to tell the world about him. Or at least the console.
+*/
+
+
 var name = "Mario";
 var job = "Plumber";
 
