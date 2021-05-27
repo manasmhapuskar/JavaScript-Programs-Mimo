3bf755d8a758f8cd94214e3ea25eee0f2fc09f2d
= <manasmhapuskar@gmail.com>
2021-05-26 22:05:39 -0500

    Deleted Notes files, Added the Problem Question in the HTML file

---
+++ 01_Basics/02_Using_Variables/01-miles-to-km-converter.js
@@ -0,0 +1,14 @@
+/*
+Problem:
+Have you tried to convert miles to kilometers or the other way around?
+Spoiler alert: It's tough.
+
+We'll make it easier by building a converter app
+to solve this problem for good.
+*/
+
+
+var miles = 50;
+var kilometers = miles * 1.609344;
+
+console.log(kilometers);
--- 01_Basics/02_Using_Variables/02-miles-to-km-converter.js
+++
@@ -1,4 +0,0 @@
-var miles = 50;
-var kilometers = miles * 1.609344;
-
-console.log(kilometers);
