c07fc9511e5fe2c2364a94a768db3d5a793e55ac
= <manasmhapuskar@gmail.com>
2021-05-28 23:28:48 -0500

    Deleted Notes files, Added the Problem Question in the HTML file

--- 01_Basics/04_Checking_Number_Equality/01-rating-app.js
+++ 01_Basics/04_Checking_Number_Equality/01-rating-app.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+We'll write an app that makes sure that the
+items in a shopping cart have good ratings.
+
+
+Task:
+We'll use variables to store item's stars and the ratings,
+and then compare them to see where the item stands.
+*/
+
+
 var stars = 4;
 var great = 5;
 var good = 4;
--- 01_Basics/04_Checking_Number_Equality/02-flight-booking.js
+++ 01_Basics/04_Checking_Number_Equality/02-flight-booking.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+Let's help a flight booking website decide 
+if a flight is fully booked.
+
+ 
+Task:
+We'll save the maximum number of bookings and 
+the current bookings and then compare them.
+*/
+
+
 var maximum = 59;
 var seatsBooked = 60;
 var full = maximum === seatsBooked;
