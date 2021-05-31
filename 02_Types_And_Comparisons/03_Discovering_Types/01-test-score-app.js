f7bb579ee69d05e3b20f7a874af42cfdd1c423dd
= <manasmhapuskar@gmail.com>
2021-05-31 16:30:33 -0500

    Deleted Notes files, Added the Problem Question in the HTML file

--- 02_Types_And_Comparisons/03_Discovering_Types/01-test-score-app.js
+++ 02_Types_And_Comparisons/03_Discovering_Types/01-test-score-app.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+We'll create an app that evaluates the test score
+of a student to check if they pass a class.
+
+
+Task:
+We'll use variables to store a number and 
+a comparison that becomes true if the student passes
+*/
+
+
 var score = 99;
 var pass = score >= 50;
 
