7bce0cf65f49a16853ca23df9c1bc64879d98dc7
= <manasmhapuskar@gmail.com>
2021-06-01 23:02:38 -0500

    Deleted Notes files, Added the Problem Question in the HTML file

--- 03_Conditionals/01_Making_Decisions/01-playlist-app.js
+++ 03_Conditionals/01_Making_Decisions/01-playlist-app.js
@@ -1,3 +1,14 @@
+/*
+Let's use a variable to keep track of whether a song
+is already in a playlist or not for a music-streaming website.
+
+
+Task:
+We'll use the variable 'included' to display a message
+if the song is already in the playlist.
+*/
+
+
 var song = "Got me thinking";
 
 console.log("New entry: " + song);
