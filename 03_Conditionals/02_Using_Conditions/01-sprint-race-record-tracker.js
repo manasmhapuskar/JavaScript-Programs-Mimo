3129080c7cc87651bc2b4d0966cc286eccf9cdcb
= <manasmhapuskar@gmail.com>
2021-06-02 17:30:12 -0500

    Deleted Notes files, Added the Problem Question in the HTML file

--- 03_Conditionals/02_Using_Conditions/01-sprint-race-record-tracker.js
+++ 03_Conditionals/02_Using_Conditions/01-sprint-race-record-tracker.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+We'll use comparisons to build an app that tracks
+the 100-meter sprint world record. Ready, set, go!
+
+
+Task:
+We'll compare the variables 'record' and 'time'
+to check if the record is broken.
+*/
+
+
 var record = 9.69;
 var time = 9.58;
 
