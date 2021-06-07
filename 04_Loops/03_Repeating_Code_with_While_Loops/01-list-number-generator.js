effa69610ad8055bde51efacc86ce6c3790e47f1
= <manasmhapuskar@gmail.com>
2021-06-07 15:02:08 -0500

    Deleted Notes files, Added the Problem Question in the HTML file

--- 04_Loops/03_Repeating_Code_with_While_Loops/01-list-number-generator.js
+++ 04_Loops/03_Repeating_Code_with_While_Loops/01-list-number-generator.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+Let's write a program that display numbers that
+displays numbers that count upwards in a list. From 1., 2., 3. and so on.
+
+
+Task:
+We'll create a variable named 'number' outside of the loop that keeps track of numbers.
+Then use it to display messages inside the loop.
+*/
+
+
 var number = 1;
 
 while (false) {
