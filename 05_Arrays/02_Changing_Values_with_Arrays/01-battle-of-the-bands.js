244c312c43aa22a31c24d925bb921bba1aa84d82
= <manasmhapuskar@gmail.com>
2021-06-12 15:23:54 -0500

    Deleted Notes files, Added the Problem Question in the HTML file

--- 05_Arrays/02_Changing_Values_with_Arrays/01-battle-of-the-bands.js
+++ 05_Arrays/02_Changing_Values_with_Arrays/01-battle-of-the-bands.js
@@ -1,3 +1,25 @@
+/*
+Problem:
+Let's practice our knowledge of arrays and indices
+by helping an event set up a battle of the bands.
+
+
+Task:
+The event managed to store the bands in an array 
+but failed to arrange who's to battle whom.
+
+We'll give them a hand by updating 'show1' with bands at even indices 
+and 'show2' with bands at odd indices.
+
+
+Available Code:
+var show1 = "TBA";
+var show2 = "TBA";
+
+var act = ["DC/AC", "King", "Stagnant Boulders", "The Pupae"];
+*/
+
+
 var show1 = "TBA";
 var show2 = "TBA";
 
--- 05_Arrays/02_Changing_Values_with_Arrays/02-jeans-shop.js
+++ 05_Arrays/02_Changing_Values_with_Arrays/02-jeans-shop.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+Let's use our knowledge of arrays to manage
+the stock of an online jeans shop.
+
+
+Task:
+The variable 'stock' stores the number of items in stocks for sizes 's', 'm', 'l'.
+Using indices, we'll update the stock values for purchases and returns.
+*/
+
+
 var s = 20;
 var m = 30;
 var l = 40;
