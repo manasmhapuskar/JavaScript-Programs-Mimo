fd2d093db7fe77f3ad87b8977a0416ef8747d189
= <manasmhapuskar@gmail.com>
2021-06-15 23:47:00 +0530

    Deleted Notes files, Added the Problem Question in the HTML file

--- 05_Arrays/04_Deciding_with_Arrays/01-pokemon-tracker.js
+++ 05_Arrays/04_Deciding_with_Arrays/01-pokemon-tracker.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+Let's put your knowledge about arrays 
+and how to make decisions with them to the test.
+
+
+Task:
+We'll create an array to keep track of all our pokemon
+and use a 'for' loop to go over the list.
+*/
+
+
 var pokemon = ["Balbasaur", "Charmandar", "Pikachu"];
 
 console.log("Pokemon in Pokedex:");
