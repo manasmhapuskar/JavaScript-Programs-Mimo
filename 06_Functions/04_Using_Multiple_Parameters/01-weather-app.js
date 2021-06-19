59835cf4aa772fb6008cf6155457412fc6b46fa8
= <manasmhapuskar@gmail.com>
2021-06-19 11:32:10 +0530

    Deleted Notes files, Added the Problem Question in the HTML file

--- 06_Functions/04_Using_Multiple_Parameters/01-weather-app.js
+++ 06_Functions/04_Using_Multiple_Parameters/01-weather-app.js
@@ -1,3 +1,18 @@
+/*
+Problem:
+Let's use our new knowledge on functions with multiple parameters
+to write code for a weather app.
+
+By using a function, we'll take advantage of multiple parameters 
+to display a summary of the temperatures for today.
+
+
+Task:
+We'll use a parameter called 'high' to display the highest
+temperature and a parameter called 'low' to display the lowest.
+*/
+
+
 function showWeather(low, high) {
     console.log("Display Weather: ");
     console.log("High: " + high);
