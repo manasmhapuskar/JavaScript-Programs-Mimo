7341f6e4193e86d236443bee301e85419d70901c
= <manasmhapuskar@gmail.com>
2021-06-22 23:51:20 +0530

    Deleted Notes files, Added the Problem Question in the HTML file

--- 08_Classes/01_Using_Classes/01-role-playing-video-game.js
+++ 08_Classes/01_Using_Classes/01-role-playing-video-game.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+We'll work on a role-playing video game where players can choose
+what type of character they want to be.
+
+
+Task:
+Since there'll be a lot of players with similar but distinct attributes,
+we'll create them as objects with the help of 'class'.
+*/
+
+
 class Character {
     constructor(name, type) {
         this.name = name;
