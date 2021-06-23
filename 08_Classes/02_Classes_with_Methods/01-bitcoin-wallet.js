45feb85ac8057b5c33e0b8510add85a305084bdb
= <manasmhapuskar@gmail.com>
2021-06-23 15:29:23 +0530

    Deleted Notes files, Added the Problem Question in the HTML file

--- 08_Classes/02_Classes_with_Methods/01-bitcoin-wallet.js
+++ 08_Classes/02_Classes_with_Methods/01-bitcoin-wallet.js
@@ -1,3 +1,18 @@
+/*
+Problem:
+Let's use our knowledge of Classes with Methods
+to code a Bitcoin wallet.
+
+
+Task:
+We'll create a 'BitcoinWallet' class 
+that keeps track of the number of bitcoins inside.
+
+We'll use a class method to handle updating
+the number of bitcoins when they get added.
+*/
+
+
 class BitcoinWallet {
     constructor(bitcoin) {
         this.bitcoin = bitcoin;
