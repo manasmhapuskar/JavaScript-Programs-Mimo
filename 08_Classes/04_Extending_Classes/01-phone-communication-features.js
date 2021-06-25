d4d1708fde50f4bb8c6c5cc0c7f624f3f1b6057c
= <manasmhapuskar@gmail.com>
2021-06-25 20:10:38 +0530

    Deleted Notes files, Added the Problem Question in the HTML file

--- 08_Classes/04_Extending_Classes/01-phone-communication-features.js
+++ 08_Classes/04_Extending_Classes/01-phone-communication-features.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+Alex is developing a life simulation game and
+wants to introduce a phone communication feature.
+
+
+Task:
+We'll help her by introducing a virtual phone device and
+a smartphone so that the characters can communicate from a distance.
+*/
+
+
 class Phone {
     call(user) {
         console.log("Calling " + user);
