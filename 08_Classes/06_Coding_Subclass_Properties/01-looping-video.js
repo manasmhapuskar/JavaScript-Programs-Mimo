6b35792afca5ca8c579ec49807c1387266b0ba5a
= <manasmhapuskar@gmail.com>
2021-06-27 23:27:28 +0530

    Deleted Notes files, Added the Problem Question in the HTML file

--- 08_Classes/06_Coding_Subclass_Properties/01-looping-video.js
+++ 08_Classes/06_Coding_Subclass_Properties/01-looping-video.js
@@ -1,3 +1,16 @@
+/*
+Problem:
+Time to practice our knowledge of Subclass Properties.
+We'll work on a social media app that shares photos and videos.
+
+
+Task:
+The team is working on a new feature that allows looping videos.
+We'll help them by making a 'Loop' subclass of a superclass called 'Video'.
+Then we'll add properties for start and end times.
+*/
+
+
 class Video {
     constructor(length) {
         this.length = length;
