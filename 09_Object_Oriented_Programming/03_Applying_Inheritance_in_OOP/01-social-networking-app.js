f3e31e16ed7514f7037141f493b7a90cffef3ac3
= <manasmhapuskar@gmail.com>
2021-07-01 23:20:41 +0530

    Deleted Notes files, Added the Problem Question in the HTML file

--- 09_Object_Oriented_Programming/03_Applying_Inheritance_in_OOP/01-social-networking-app.js
+++ 09_Object_Oriented_Programming/03_Applying_Inheritance_in_OOP/01-social-networking-app.js
@@ -1,3 +1,43 @@
+/*
+Problem:
+Let's practice our knowledge of inheritance by fixing some code.
+
+
+Available Code:
+Take a look at this code for a social networking app 
+that lets users post their current status.
+function createUser(name) {
+    var user = {
+        name: name,
+        status: '',
+        updateStatus: function(status) {
+            user.status = status;
+        }
+    };
+    
+    return user;
+}
+
+var user1 = createUser("Matt");
+var user2 = createUser("Mary");
+var user3 = createUser("John");
+
+user1.updateStatus("Out fishing");
+
+console.log(user1);
+console.log(user2);
+console.log(user3);
+
+
+Task:
+A developer wrote a 'createUser()' function to create new user objects.
+The problem is that each user has its own 'updateStatus()' method.
+
+If too many users sign up, the server could crash.
+Let's fix that by making the code use classes.
+*/
+
+
 class User {
     constructor(name) {
         this.name = name;
