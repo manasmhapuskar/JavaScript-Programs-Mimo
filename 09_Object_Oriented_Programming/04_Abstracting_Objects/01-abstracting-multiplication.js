1c9af9bf1c697d23c1067f3cc7c0d77a32fb7bc5
manasmhapuskar <manasmhapuskar@gmail.com>
2021-07-02 21:20:38 +0530

    Deleted Notes files, Added the Problem Question in the HTML file

--- 09_Object_Oriented_Programming/04_Abstracting_Objects/01-abstracting-multiplication.js
+++ 09_Object_Oriented_Programming/04_Abstracting_Objects/01-abstracting-multiplication.js
@@ -1,3 +1,33 @@
+/*
+Problem:
+Early calculators could only perform one operation: addition.
+To compute 7 times 3, for example, users had to add repeatedly.
+
+
+Task:
+Let's improve this calculator by coding 'multiply()' method
+that abstracts away these repeated 'add()' calls.
+
+ 
+Available Code:
+
+class Calculator {
+    constructor(value) {
+        this.value = value;
+    }
+    
+    add(num) {
+        this.value += num;
+    }
+}
+
+var calculator = new Calculator(7);
+calculator.add(7);
+calculator.add(7);
+console.log(calculator);
+*/
+
+
 class Calculator {
     constructor(value) {
         this.value = value;
