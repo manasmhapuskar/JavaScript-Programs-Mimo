ebd8f07414627d5b3d8aa4be6ae6b715d859f106
manasmhapuskar <manasmhapuskar@gmail.com>
2021-07-09 14:46:14 +0530

    Deleted Notes files, Added the Problem Question in the HTML file

--- 10_Dynamic_Webpages/06_Including_a_Script/01-build-an-online-store.js
+++ 10_Dynamic_Webpages/06_Including_a_Script/01-build-an-online-store.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+Let's use Interacting with Webpages to set up an interactive
+webpage for an online store.
+
+
+Task:
+We'll code a basic sales page and write a function
+that updates the item on sale dynamically.
+*/
+
+
 var saleItem = "Shirt";
 
 function updateSale() {
--- 10_Dynamic_Webpages/06_Including_a_Script/02-code-a-username-generator.js
+++ 10_Dynamic_Webpages/06_Including_a_Script/02-code-a-username-generator.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+Let's use the interacting with Webpages 
+to create a button that generates a user for a user.
+
+
+Task:
+We'll make a webpage with some user details and space for the username.
+Then we'll add the function to generate the username.
+*/
+
+
 function generate() {
     var name = document.getElementById("name").innerHTML;
     var age = document.getElementById("age").innerHTML;
