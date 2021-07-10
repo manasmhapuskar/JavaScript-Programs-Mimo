eb104e112b5d3e980d2cdb8d6632f38f2e735210
manasmhapuskar <manasmhapuskar@gmail.com>
2021-07-10 21:19:21 +0530

    Deleted Notes files, Added the Problem Question in the HTML file

--- 10_Dynamic_Webpages/07_Getting_User_Input/01-getting-user-input.js
+++ 10_Dynamic_Webpages/07_Getting_User_Input/01-getting-user-input.js
@@ -1,3 +1,15 @@
+/*
+Problem:
+Let's apply our knowledge of user input to build a signup page for Snapcat,
+an app for sending disappearing pictures of cats.
+
+
+Part II
+With the Snapcat webpage in place, let's make it interactive by accessing
+and updating the values through an included script.
+*/
+
+
 function signUp() {
     var username = document.getElementById("usernameInput").value;
     var email = document.getElementById("emailInput").value;
