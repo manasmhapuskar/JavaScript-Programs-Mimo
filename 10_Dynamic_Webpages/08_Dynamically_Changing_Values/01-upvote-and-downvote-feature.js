6062c4c96ec27be5bba71748f9f0a31dd4b4e2de
= <manasmhapuskar@gmail.com>
2021-07-11 23:27:18 +0530

    Deleted Notes files, Added the Problem Question in the HTML file

--- 10_Dynamic_Webpages/08_Dynamically_Changing_Values/01-upvote-and-downvote-feature.js
+++ 10_Dynamic_Webpages/08_Dynamically_Changing_Values/01-upvote-and-downvote-feature.js
@@ -1,3 +1,21 @@
+/*
+Problem:
+In this project, we'll code a webpage with upvote and downvote buttons
+to increase and decrease the number of votes on a photos.
+
+
+Task:
+Part II
+You've got the structure down for your webpage.
+Now it's time to put your JavaScript skills to the test.
+You'll code a button that calls the 'upvote()' function when it's clicked.
+
+Part III
+Your upvoting feature works great.
+But for those who disagree, you'll need to build a downvote button.
+*/
+
+
 var counter = 3;
 
 function upVote() {
