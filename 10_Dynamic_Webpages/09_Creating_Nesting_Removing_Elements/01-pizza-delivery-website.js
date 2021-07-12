13a9e516316fbd299592477bf818300eb6531287
= <manasmhapuskar@gmail.com>
2021-07-12 20:40:15 +0530

    Deleted Notes files, Added the Problem Question in the HTML file

--- 10_Dynamic_Webpages/09_Creating_Nesting_Removing_Elements/01-pizza-delivery-website.js
+++ 10_Dynamic_Webpages/09_Creating_Nesting_Removing_Elements/01-pizza-delivery-website.js
@@ -1,3 +1,21 @@
+/*
+Problem:
+In this project, we'll update the incoming orders of a pizza delivery
+service when the page refreshes.
+Other than adding new orders, 
+our code should move pizzas ready for delivery for the Ready list.
+
+ 
+Task:
+Part I
+We'll manage the order list by creating HTML elements with JavaScript
+    
+Part II
+To move an element between the Order and Ready lists,
+we delete the element from the first list and then add it to the second.
+*/
+
+
 var orderList = document.getElementById("orders");
 
 var marinara = document.createElement("li");
