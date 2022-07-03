d22aaa69ff1b5f4a20a0dbeed46d5325a8fc1ec9
= <manasmhapuskar@gmail.com>
2022-07-03 23:53:45 +0530

    Removed line spaces

--- 01_Basics/02_Using_Variables/scripts/01-game-profile.js
+++ 01_Basics/02_Using_Variables/scripts/01-game-profile.js
@@ -2,7 +2,6 @@ var firstName = "Mike ";
 var secondName = "Tyson";
 var userName = firstName + secondName;
 console.log("Username: " + userName);
-
 var characterName = "Dr. Doom";
 var character = characterName;
 console.log("Character: " + character);
