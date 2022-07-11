c0eb6262b924c19aae422bbaee8b950f0e488510
= <manasmhapuskar@gmail.com>
2022-07-11 23:22:32 +0530

    Removed line spaces

--- 02_Types_And_Comparisons/01_Comparing_Numbers/01-measure-heart-rate.js
+++ 02_Types_And_Comparisons/01_Comparing_Numbers/01-measure-heart-rate.js
@@ -12,6 +12,5 @@ too high and tell the patient if they should worry about their health.
 var heartRate = 77;
 var tooLow = heartRate < 60;
 var tooHigh = heartRate > 100;
-
 console.log("Heart rate low: " + tooLow);
 console.log("Heart rate high: " + tooHigh);
