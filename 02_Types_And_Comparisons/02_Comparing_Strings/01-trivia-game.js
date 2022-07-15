cf466680ef9ddb48c3e8d2d8b8042ecf38fb8d96
= <manasmhapuskar@gmail.com>
2022-07-15 23:07:52 +0530

    Removed line spaces

--- 02_Types_And_Comparisons/02_Comparing_Strings/01-trivia-game.js
+++ 02_Types_And_Comparisons/02_Comparing_Strings/01-trivia-game.js
@@ -15,7 +15,6 @@ var correctAnswer = "blue";
 var firstAnswer = "red";
 console.log("Is the first answer correct?");
 console.log(correctAnswer === firstAnswer);
-
 var secondAnswer = "blue";
 console.log("Is the second answer correct?");
 console.log(correctAnswer === secondAnswer);
