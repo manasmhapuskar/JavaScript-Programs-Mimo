80bf6b26744fb026ef3d154f5549f54135d085cc
= <manasmhapuskar@gmail.com>
2022-07-29 23:56:05 +0530

    Removed line spaces

--- 03_Conditionals/01_Making_Decisions/01-playlist-app.js
+++ 03_Conditionals/01_Making_Decisions/01-playlist-app.js
@@ -14,7 +14,6 @@ var song = "Got me thinking";
 console.log("New entry: " + song);
 
 var isSongInPlaylist = "Got me thinking" === song;
-
 if (isSongInPlaylist) {
     console.log("This song already exists in the Playlist");
 }
