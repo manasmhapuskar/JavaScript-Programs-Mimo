77342d7c24cb77fd247d4b140c6d49c360fbe2ba
= <manasmhapuskar@gmail.com>
2022-07-30 00:19:26 +0530

    Removed line spaces

--- 03_Conditionals/01_Making_Decisions/01-playlist-app.js
+++ 03_Conditionals/01_Making_Decisions/01-playlist-app.js
@@ -12,7 +12,6 @@ if the song is already in the playlist.
 var song = "Got me thinking";
 
 console.log("New entry: " + song);
-
 var isSongInPlaylist = "Got me thinking" === song;
 if (isSongInPlaylist) {
     console.log("This song already exists in the Playlist");
